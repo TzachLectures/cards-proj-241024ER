2e3c12c6c4b428d351b285bf81d1a122be3213fc
TzachLectures <tzachd1233@gmail.com>
2025-05-25 18:06:29 +0300

    seperate bcard to components

--- src/cards/components/BCard.jsx
+++ src/cards/components/BCard.jsx
@@ -1,17 +1,6 @@
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Divider,
-  Typography,
-  IconButton,
-  Box,
-} from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import CallIcon from "@mui/icons-material/Call";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Card, CardMedia } from "@mui/material";
+import BCardBody from "./BCardBody";
+import BCardFooter from "./BCardFooter";
 
 function BCard({ card }) {
   return (
@@ -21,42 +10,14 @@ function BCard({ card }) {
         image={card.image.url}
         title="Businees logo"
       />
-      <CardContent>
-        <Typography variant="h5">{card.title}</Typography>
-        <Typography variant="h6">{card.subtitle}</Typography>
-        <Divider sx={{ marginY: 1 }} />
-        <Typography variant="body2">
-          <strong>Phone:</strong> {card.phone}
-        </Typography>
-        <Typography variant="body2">
-          <strong>Address:</strong> {card.address.city}
-        </Typography>
-        <Typography variant="body2">Card Number: {card.bizNumber}</Typography>
-      </CardContent>
-      <CardActions
-        sx={{ display: "flex", justifyContent: "space-between" }}
-        disableSpacing
-      >
-        <Box>
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-
-          <IconButton>
-            <EditIcon />
-          </IconButton>
-        </Box>
-
-        <Box>
-          <IconButton>
-            <CallIcon />
-          </IconButton>
-
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-        </Box>
-      </CardActions>
+      <BCardBody
+        title={card.title}
+        subtitle={card.subtitle}
+        bizNumber={card.bizNumber}
+        phone={card.phone}
+        city={card.address.city}
+      />
+      <BCardFooter />
     </Card>
   );
 }
