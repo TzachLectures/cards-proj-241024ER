14c6acd3c6944c196a0b88599ca2ed21cc6b18f7
TzachLectures <tzachd1233@gmail.com>
2025-06-04 21:18:25 +0300

    add use form

--- src/users/components/RegisterForm.jsx
+++ src/users/components/RegisterForm.jsx
@@ -1,51 +1,16 @@
 import { Button, Container, TextField } from "@mui/material";
-import { useState } from "react";
 import Joi from "joi";
+import useForm from "../../hooks/useForm";
 
 function RegisterForm() {
-  const [userDetails, setUserDetails] = useState({
-    firstName: "",
-    lastName: "",
-    middleName: "",
-  });
-
-  const [errors, setErrors] = useState({});
-
-  const schema = Joi.object({
-    firstName: Joi.string().min(2).max(10),
-    middleName: Joi.string().allow(""),
-    lastName: Joi.string().min(2).required(),
-  });
-
-  const handleChange = (e) => {
-    const fieldName = e.target.name;
-    const fieldValue = e.target.value;
-    setUserDetails((prev) => ({
-      ...prev,
-      [fieldName]: fieldValue,
-    }));
-
-    if (fieldName === "firstName") {
-      const firstNameSchema = Joi.object({
-        firstName: Joi.string().min(2).max(10),
-      });
-      const { error } = firstNameSchema.validate({ firstName: fieldValue });
-      if (error) {
-        setErrors({ firstName: error.details[0].message });
-      } else {
-        setErrors((prev) => {
-          delete prev.firstName;
-          return prev;
-        });
-      }
+  const { formDetails, errors, handleChange, handleSubmit } = useForm(
+    { firstName: "", lastName: "", middleName: "" },
+    {
+      firstName: Joi.string().min(2).max(10),
+      middleName: Joi.string().allow(""),
+      lastName: Joi.string().min(2).required(),
     }
-  };
-
-  const handleSignup = () => {
-    console.log(userDetails);
-    const { error } = schema.validate(userDetails, { abortEarly: false });
-    console.log(error);
-  };
+  );
 
   return (
     <Container
@@ -64,13 +29,21 @@ function RegisterForm() {
         label={"middle name"}
         name="middleName"
         onChange={handleChange}
+        error={Boolean(errors.middleName)}
+        helperText={errors.middleName}
       />
-      <TextField label={"last name"} name="lastName" onChange={handleChange} />
+      <TextField
+        label={"last name"}
+        name="lastName"
+        onChange={handleChange}
+        error={Boolean(errors.lastName)}
+        helperText={errors.lastName}
+      />
 
       <Button
         variant="contained"
         sx={{ display: "block" }}
-        onClick={handleSignup}
+        onClick={handleSubmit}
       >
         Sign up
       </Button>
